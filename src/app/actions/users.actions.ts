bce1b775c59a93a2baad54b88a305fec5ec30769
ugrasenanv <ugrasenanv@gmail.com>
2020-02-09 17:03:07 +0530

    2 files modified

--- src/app/actions/users.actions.ts
+++ src/app/actions/users.actions.ts
@@ -1,34 +1,48 @@
-import { Injectable } from '@angular/core';
-import { IAppState } from '../store/index';
-import { NgRedux } from '@angular-redux/store';
-import { User } from '../model/user';
+import { Injectable } from "@angular/core";
+import { IAppState } from "../store/index";
+import { NgRedux } from "@angular-redux/store";
+import { User } from "../model/user";
 
 @Injectable()
 export class UsersActions {
-  static USER_ADD = 'USER_ADD';
-  static USER_DELETE = 'USER_DELETE';
+  static USER_ADD = "USER_ADD";
+  static USER_DELETE = "USER_DELETE";
 
-  constructor(
-    private ngRedux: NgRedux<IAppState>,
-  ) { }
+  constructor(private ngRedux: NgRedux<IAppState>) {}
 
   add(obj: any): void {
-     console.log('obj=====>',obj);
+    console.log("obj=====>", obj);
     // console.log(obj)
     this.ngRedux.dispatch({
       type: UsersActions.USER_ADD,
       payload: {
         id: new Date().valueOf(), // random id
-        obj
+        firstName: obj.firstName,
+        lastName: obj.lastName,
+        email: obj.email,
+        gender: obj.gender,
+        phoneNumber: obj.phoneNumber,
+        comments: obj.comments,
+        rating: obj.rating,
+        date: obj.date
       }
     });
   }
-  
+
   delete(id): void {
     this.ngRedux.dispatch({
       type: UsersActions.USER_DELETE,
       payload: id
     });
   }
-
 }
+
+//  id?: number;
+//   firstName?: string;
+//   lastName?: string;
+//   email?: string;
+//   gender?: string;
+//   phoneNumber?: string;
+//   comments?: string;
+//   rating?: number;
+//   date?: string;
